c59aa3e13d21dbe87b792bac26f6bcd0239bc6c9
Emil Jasinski <emiljasinski@gmail.com>
2014-10-29 00:31:48 +0100

    final second example

--- webgl/arms.js
+++ webgl/arms.js
@@ -140,61 +140,80 @@ var Arms;
     var armPositionLocation;
     var armViewMatrixLocation;
     var armMovementMatrixLocation;
+    var greaterD = 0.09;
+    var smallerD = 0.05;
+    var arm1L = 0.9;
     var arm1VertexBuffer = [
-        0.25,
-        0.0,
-        0.75,
-        0.0,
-        0.75,
-        1.0,
-        0.25,
-        1.0,
+        -greaterD,
+        -smallerD,
+        arm1L - greaterD,
+        -smallerD,
+        arm1L - greaterD,
+        smallerD,
+        -greaterD,
+        smallerD,
     ];
     var arm1FacesBuffer = [0, 1, 2, 2, 3, 0];
+    var arm2L = 0.75;
     var arm2VertexBuffer = [
-        -1.0,
-        -1.0,
-        -0.7,
-        -1.0,
-        -1.0,
-        -0.7,
+        -smallerD,
+        -greaterD,
+        smallerD,
+        -greaterD,
+        smallerD,
+        arm2L - greaterD,
+        -smallerD,
+        arm2L - greaterD,
     ];
-    var arm2FacesBuffer = [0, 1, 2];
+    var arm2FacesBuffer = [0, 1, 2, 2, 3, 0];
     function main() {
         var isLeftButtonDown = false;
-        var leftButtonPosition = { x: 0, y: 0 };
+        var leftActionPosition = { x: 0, y: 0 };
+        var rotate = { x: 0, y: 0 };
         var isRightButtonDown = false;
-        var rightButtonPosition = { x: 0, y: 0 };
+        var rightActionPosition = { x: 0, y: 0 };
+        var angle1 = 15;
+        var angle2 = 15;
+        var translateX = 0;
+        var translateY = 0;
         canvas.oncontextmenu = function (e) {
             e.preventDefault();
         };
         canvas.addEventListener("pointerdown", function (e) {
-            console.log(e.offsetX, e.offsetY);
+            leftActionPosition = { x: e.offsetX, y: e.offsetY };
             if (isLeftButton(e))
                 isLeftButtonDown = true;
             if (isRightButton(e))
                 isRightButtonDown = true;
         }, false);
         canvas.addEventListener("pointermove", function (e) {
+            var x = e.offsetX;
+            var y = e.offsetY;
             if (isLeftButtonDown) {
-                console.log("left");
+                translateX += -(leftActionPosition.x - x) / 250;
+                translateY += (leftActionPosition.y - y) / 250;
+                leftActionPosition.x = x;
+                leftActionPosition.y = y;
             }
             if (isRightButtonDown) {
-                console.log("right");
+                angle1 += (rightActionPosition.x - x);
+                angle2 += (rightActionPosition.y - y);
+                rightActionPosition.x = x;
+                rightActionPosition.y = y;
             }
         }, false);
         canvas.addEventListener("pointerup", function (e) {
-            if (isLeftButton(e))
-                isLeftButtonDown = false;
-            if (isRightButton(e))
-                isRightButtonDown = false;
+            endPointerEvent(e);
         }, false);
         canvas.addEventListener("pointerleave", function (e) {
+            endPointerEvent(e);
+        }, false);
+        function endPointerEvent(e) {
             if (isLeftButton(e))
                 isLeftButtonDown = false;
             if (isRightButton(e))
                 isRightButtonDown = false;
-        }, false);
+        }
         function isLeftButton(e) {
             return e.button === 0;
         }
@@ -230,20 +249,26 @@ var Arms;
         var oldTime = 0;
         function animate(time) {
             var dAngle = 0.25 * (time - oldTime);
+            var tempMovementMatrix1 = mat4.create();
+            var tempMovementMatrix2 = mat4.create();
+            mat4.translate(tempMovementMatrix1, movementMatrix1, [translateX, translateY, 0]);
+            mat4.rotate(tempMovementMatrix1, tempMovementMatrix1, glMatrix.toRadian(angle1), [0, 0, 1]);
+            mat4.translate(tempMovementMatrix2, movementMatrix2, [translateX, translateY, 0]);
+            mat4.rotate(tempMovementMatrix2, tempMovementMatrix2, glMatrix.toRadian(angle2), [0, 0, 1]);
             oldTime = time;
             gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
             gl.bindBuffer(gl.ARRAY_BUFFER, arm1Vertex);
             gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, arm1Faces);
             gl.uniformMatrix4fv(armViewMatrixLocation, false, viewMatrix1);
-            gl.uniformMatrix4fv(armMovementMatrixLocation, false, movementMatrix1);
+            gl.uniformMatrix4fv(armMovementMatrixLocation, false, tempMovementMatrix1);
             gl.vertexAttribPointer(armPositionLocation, 2, gl.FLOAT, false, 4 * 2, 0);
             gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0);
             gl.bindBuffer(gl.ARRAY_BUFFER, arm2Vertex);
             gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, arm2Faces);
             gl.uniformMatrix4fv(armViewMatrixLocation, false, viewMatrix2);
-            gl.uniformMatrix4fv(armMovementMatrixLocation, false, movementMatrix2);
+            gl.uniformMatrix4fv(armMovementMatrixLocation, false, tempMovementMatrix2);
             gl.vertexAttribPointer(armPositionLocation, 2, gl.FLOAT, false, 4 * 2, 0);
-            gl.drawElements(gl.TRIANGLES, 3, gl.UNSIGNED_SHORT, 0);
+            gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0);
             gl.flush();
             window.requestAnimationFrame(animate);
         }
--- webgl/arms.ts
+++ webgl/arms.ts
@@ -36,42 +36,47 @@ module Arms {
   var armViewMatrixLocation: WebGLUniformLocation;
   var armMovementMatrixLocation: WebGLUniformLocation;
 
-  // var arm1VertexBuffer = [
-  //   0.25, 0.0,
-  //   0.75, 0.0,
-  //   0.75, 1.0,
-  //   0.25, 1.0,
-  // ];
+  var greaterD = 0.09;
+  var smallerD = 0.05;
 
+  var arm1L = 0.9;
   var arm1VertexBuffer = [
-    0.25, 0.0,
-    0.75, 0.0,
-    0.75, 1.0,
-    0.25, 1.0,
+    -greaterD, -smallerD,
+    arm1L - greaterD, -smallerD,
+    arm1L - greaterD, smallerD,
+    -greaterD, smallerD,
   ];
 
   var arm1FacesBuffer = [0, 1, 2, 2, 3, 0];
 
+  var arm2L = 0.75;
   var arm2VertexBuffer = [
-    -1.0, -1.0,
-    -0.7, -1.0,
-    -1.0, -0.7,
+    -smallerD, -greaterD,
+    smallerD, -greaterD,
+    smallerD, arm2L - greaterD,
+    -smallerD, arm2L - greaterD,
   ];
 
-  var arm2FacesBuffer = [0, 1, 2];
+  var arm2FacesBuffer = [0, 1, 2, 2, 3, 0];
 
   function main() {
     var isLeftButtonDown = false;
-    var leftButtonPosition = { x: 0, y: 0 };
+    var leftActionPosition = { x: 0, y: 0 };
+    var rotate = { x: 0, y: 0 };
     var isRightButtonDown = false;
-    var rightButtonPosition = { x: 0, y: 0 };
+    var rightActionPosition = { x: 0, y: 0 };
+    var angle1 = 15;
+    var angle2 = 15;
+    var translateX = 0;
+    var translateY = 0;
 
     canvas.oncontextmenu = function(e) {
       e.preventDefault();
     }
 
     canvas.addEventListener("pointerdown", function(e) {
-      console.log(e.offsetX, e.offsetY);
+      leftActionPosition = { x: e.offsetX, y: e.offsetY };
+
       if(isLeftButton(e))
         isLeftButtonDown = true;
       if(isRightButton(e))
@@ -79,27 +84,38 @@ module Arms {
     }, false);
 
     canvas.addEventListener("pointermove", function(e) {
+      var x = e.offsetX;
+      var y = e.offsetY;
+
       if(isLeftButtonDown) {
-        console.log("left");
+        translateX += -(leftActionPosition.x - x) / 250;
+        translateY += (leftActionPosition.y - y) / 250;
+        leftActionPosition.x = x;
+        leftActionPosition.y = y;
+
       }
       if(isRightButtonDown) {
-        console.log("right");
+        angle1 += (rightActionPosition.x - x);
+        angle2 += (rightActionPosition.y - y);
+        rightActionPosition.x = x;
+        rightActionPosition.y = y;
       }
     }, false);
 
     canvas.addEventListener("pointerup", function(e) {
-      if(isLeftButton(e))
-        isLeftButtonDown = false;
-      if(isRightButton(e))
-        isRightButtonDown = false;
+      endPointerEvent(e);
     }, false);
 
     canvas.addEventListener("pointerleave", function(e) {
+      endPointerEvent(e);
+    }, false)
+
+    function endPointerEvent(e) {
       if(isLeftButton(e))
         isLeftButtonDown = false;
       if(isRightButton(e))
         isRightButtonDown = false;
-    }, false)
+    }
 
     function isLeftButton(e: MouseEvent) {
       return e.button === 0;
@@ -147,21 +163,29 @@ module Arms {
     var movementMatrix1 = mat4.create();
     mat4.identity(movementMatrix1);
 
-    // mat4.translate(viewMatrix1, viewMatrix1, [0, 0, -1]);
-
     var viewMatrix2 = mat4.create();
     mat4.identity(viewMatrix2);
 
     var movementMatrix2 = mat4.create();
     mat4.identity(movementMatrix2);
 
-    // mat4.translate(viewMatrix2, viewMatrix2, [0, 0, -3]);
+    // mat4.translate(viewMatrix1, viewMatrix1, [0, 0, 0]);
+    // mat4.translate(viewMatrix2, viewMatrix2, [0, 0, 0]);
 
     var oldTime = 0;
 
     function animate(time) {
       var dAngle = 0.25 * (time - oldTime);
-      // mat4.rotate(movementMatrix1, movementMatrix1, glMatrix.toRadian(dAngle), [0, 1, 0]);
+
+      var tempMovementMatrix1 = mat4.create();
+      var tempMovementMatrix2 = mat4.create();
+
+      mat4.translate(tempMovementMatrix1, movementMatrix1, [translateX, translateY, 0]);
+      mat4.rotate(tempMovementMatrix1, tempMovementMatrix1, glMatrix.toRadian(angle1), [0, 0, 1]);
+      
+      mat4.translate(tempMovementMatrix2, movementMatrix2, [translateX, translateY, 0]);
+      mat4.rotate(tempMovementMatrix2, tempMovementMatrix2, glMatrix.toRadian(angle2), [0, 0, 1]);
+
 
       oldTime = time;
       gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
@@ -169,16 +193,16 @@ module Arms {
       gl.bindBuffer(gl.ARRAY_BUFFER, arm1Vertex);
       gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, arm1Faces);
       gl.uniformMatrix4fv(armViewMatrixLocation, false, viewMatrix1);
-      gl.uniformMatrix4fv(armMovementMatrixLocation, false, movementMatrix1);
+      gl.uniformMatrix4fv(armMovementMatrixLocation, false, tempMovementMatrix1);
       gl.vertexAttribPointer(armPositionLocation, 2, gl.FLOAT, false, 4*2, 0);
       gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0);
 
       gl.bindBuffer(gl.ARRAY_BUFFER, arm2Vertex);
       gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, arm2Faces);
       gl.uniformMatrix4fv(armViewMatrixLocation, false, viewMatrix2);
-      gl.uniformMatrix4fv(armMovementMatrixLocation, false, movementMatrix2);
+      gl.uniformMatrix4fv(armMovementMatrixLocation, false, tempMovementMatrix2);
       gl.vertexAttribPointer(armPositionLocation, 2, gl.FLOAT, false, 4*2, 0);
-      gl.drawElements(gl.TRIANGLES, 3, gl.UNSIGNED_SHORT, 0);
+      gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0);
 
       gl.flush();
       window.requestAnimationFrame(animate);
