146bf63d7f1c91214047226b5a37412a06254f28
Emil Jasinski <emiljasinski@gmail.com>
2014-10-28 08:28:59 +0100

    webgl touch

--- webgl/arms.ts
+++ webgl/arms.ts
@@ -21,11 +21,10 @@ module Arms {
 
   var vertexShaderSource = "\n\
   attribute vec2 aPosition;\n\
-  uniform mat4 uProjectionMatrix;\n\
   uniform mat4 uViewMatrix;\n\
   uniform mat4 uMovementMatrix;\n\
   void main(void) {\n\
-    gl_Position = uProjectionMatrix * uViewMatrix * uMovementMatrix * vec4(aPosition, 0.0, 1.0);\n\
+    gl_Position = uViewMatrix * uMovementMatrix * vec4(aPosition, 0.0, 1.0);\n\
   }";
 
   var fragmentShaderSource = "\n\
@@ -33,16 +32,16 @@ module Arms {
     gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n\
   }";
 
-  // var vertexShaderSource2 = "\n\
-  // attribute vec2 aPosition;\n\
-  // void main(void) {\n\
-  //   gl_Position = vec4(aPosition, 0.0, 1.0);\n\
-  // }";
+  var armPositionLocation: number;
+  var armViewMatrixLocation: WebGLUniformLocation;
+  var armMovementMatrixLocation: WebGLUniformLocation;
 
-  // var fragmentShaderSource2 = "\n\
-  // void main(void) {\n\
-  //   gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n\
-  // }";
+  // var arm1VertexBuffer = [
+  //   0.25, 0.0,
+  //   0.75, 0.0,
+  //   0.75, 1.0,
+  //   0.25, 1.0,
+  // ];
 
   var arm1VertexBuffer = [
     0.25, 0.0,
@@ -53,11 +52,6 @@ module Arms {
 
   var arm1FacesBuffer = [0, 1, 2, 2, 3, 0];
 
-  var arm1PositionLocation: number;
-  var arm1ProjectionMatrixLocation: WebGLUniformLocation;
-  var arm1ViewMatrixLocation: WebGLUniformLocation;
-  var arm1MovementMatrixLocation: WebGLUniformLocation;
-
   var arm2VertexBuffer = [
     -1.0, -1.0,
     -0.7, -1.0,
@@ -66,12 +60,55 @@ module Arms {
 
   var arm2FacesBuffer = [0, 1, 2];
 
-  var arm2PositionLocation: number;
-  // var arm2ProjectionMatrixLocation: number;
-  // var arm2ViewMatrixLocation: number;
-  // var arm2MovementMatrixLocation: number;
-
   function main() {
+    var isLeftButtonDown = false;
+    var leftButtonPosition = { x: 0, y: 0 };
+    var isRightButtonDown = false;
+    var rightButtonPosition = { x: 0, y: 0 };
+
+    canvas.oncontextmenu = function(e) {
+      e.preventDefault();
+    }
+
+    canvas.addEventListener("pointerdown", function(e) {
+      console.log(e.offsetX, e.offsetY);
+      if(isLeftButton(e))
+        isLeftButtonDown = true;
+      if(isRightButton(e))
+        isRightButtonDown = true;
+    }, false);
+
+    canvas.addEventListener("pointermove", function(e) {
+      if(isLeftButtonDown) {
+        console.log("left");
+      }
+      if(isRightButtonDown) {
+        console.log("right");
+      }
+    }, false);
+
+    canvas.addEventListener("pointerup", function(e) {
+      if(isLeftButton(e))
+        isLeftButtonDown = false;
+      if(isRightButton(e))
+        isRightButtonDown = false;
+    }, false);
+
+    canvas.addEventListener("pointerleave", function(e) {
+      if(isLeftButton(e))
+        isLeftButtonDown = false;
+      if(isRightButton(e))
+        isRightButtonDown = false;
+    }, false)
+
+    function isLeftButton(e: MouseEvent) {
+      return e.button === 0;
+    }
+
+    function isRightButton(e: MouseEvent) {
+      return e.button === 2;
+    }
+
     initShaders();
 
     var arm1Vertex = gl.createBuffer();
@@ -104,43 +141,43 @@ module Arms {
     gl.depthFunc(gl.LEQUAL);
     gl.clearDepth(1.0);
 
-    var projectionMatrix = mat4.create();
-    mat4.perspective(projectionMatrix, 
-                    glMatrix.toRadian(40), 
-                    canvas.width / canvas.height,
-                    1, 100);
+    var viewMatrix1 = mat4.create();
+    mat4.identity(viewMatrix1);
 
-    var viewMatrix = mat4.create();
-    mat4.identity(viewMatrix);
+    var movementMatrix1 = mat4.create();
+    mat4.identity(movementMatrix1);
 
-    var movementMatrix = mat4.create();
-    mat4.identity(movementMatrix);
+    // mat4.translate(viewMatrix1, viewMatrix1, [0, 0, -1]);
 
-    mat4.translate(viewMatrix, viewMatrix, [0, 0, -5]);
+    var viewMatrix2 = mat4.create();
+    mat4.identity(viewMatrix2);
+
+    var movementMatrix2 = mat4.create();
+    mat4.identity(movementMatrix2);
+
+    // mat4.translate(viewMatrix2, viewMatrix2, [0, 0, -3]);
 
     var oldTime = 0;
 
     function animate(time) {
       var dAngle = 0.25 * (time - oldTime);
-      mat4.rotate(movementMatrix, movementMatrix, glMatrix.toRadian(dAngle), [0, 1, 0]);
+      // mat4.rotate(movementMatrix1, movementMatrix1, glMatrix.toRadian(dAngle), [0, 1, 0]);
 
       oldTime = time;
       gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
       gl.bindBuffer(gl.ARRAY_BUFFER, arm1Vertex);
       gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, arm1Faces);
-      gl.uniformMatrix4fv(arm1ProjectionMatrixLocation, false, projectionMatrix);
-      gl.uniformMatrix4fv(arm1ViewMatrixLocation, false, viewMatrix);
-      gl.uniformMatrix4fv(arm1MovementMatrixLocation, false, movementMatrix);
-      gl.vertexAttribPointer(arm1PositionLocation, 2, gl.FLOAT, false, 4*2, 0);
+      gl.uniformMatrix4fv(armViewMatrixLocation, false, viewMatrix1);
+      gl.uniformMatrix4fv(armMovementMatrixLocation, false, movementMatrix1);
+      gl.vertexAttribPointer(armPositionLocation, 2, gl.FLOAT, false, 4*2, 0);
       gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0);
 
       gl.bindBuffer(gl.ARRAY_BUFFER, arm2Vertex);
       gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, arm2Faces);
-      gl.uniformMatrix4fv(arm1ProjectionMatrixLocation, false, projectionMatrix);
-      gl.uniformMatrix4fv(arm1ViewMatrixLocation, false, viewMatrix);
-      gl.uniformMatrix4fv(arm1MovementMatrixLocation, false, movementMatrix);
-      gl.vertexAttribPointer(arm2PositionLocation, 2, gl.FLOAT, false, 4*2, 0);
+      gl.uniformMatrix4fv(armViewMatrixLocation, false, viewMatrix2);
+      gl.uniformMatrix4fv(armMovementMatrixLocation, false, movementMatrix2);
+      gl.vertexAttribPointer(armPositionLocation, 2, gl.FLOAT, false, 4*2, 0);
       gl.drawElements(gl.TRIANGLES, 3, gl.UNSIGNED_SHORT, 0);
 
       gl.flush();
@@ -150,26 +187,16 @@ module Arms {
   }
 
   function initShaders() {
-    initArm1Shader();
-    // initArm2Shader();
+    initArmShader();
   }
 
-  function initArm1Shader() {
+  function initArmShader() {
     var program = utils.useProgram(vertexShaderSource, fragmentShaderSource);
 
-    arm1PositionLocation = gl.getAttribLocation(program, "aPosition");
-    arm1ProjectionMatrixLocation = gl.getUniformLocation(program, "uProjectionMatrix");
-    arm1ViewMatrixLocation = gl.getUniformLocation(program, "uViewMatrix");
-    arm1MovementMatrixLocation = gl.getUniformLocation(program, "uMovementMatrix");
+    armPositionLocation = gl.getAttribLocation(program, "aPosition");
+    armViewMatrixLocation = gl.getUniformLocation(program, "uViewMatrix");
+    armMovementMatrixLocation = gl.getUniformLocation(program, "uMovementMatrix");
 
-    gl.enableVertexAttribArray(arm1PositionLocation);
+    gl.enableVertexAttribArray(armPositionLocation);
   }
-
-  // function initArm2Shader() {
-  //   var program = utils.useProgram(vertexShaderSource2, fragmentShaderSource2);
-
-  //   arm2PositionLocation = gl.getAttribLocation(program, "aPosition");
-
-  //   gl.enableVertexAttribArray(arm2PositionLocation);
-  // }
 }
