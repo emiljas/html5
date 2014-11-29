6f8639e5d5d1851ffac78e89bff4bf99ebae6465
emil <emiljasinski@gmail.com>
2014-11-29 21:13:48 +0100

    x

--- 2811/script.js
+++ 2811/script.js
@@ -1,32 +1,25 @@
 var canvas;
 var context;
-window.addEventListener("load", function () {
-    var worker = new Worker("worker.js");
-    worker.onmessage = function (e) {
-        console.log("a");
-    };
-    worker.postMessage("abc");
-    canvas = document.getElementById("canvas");
-    var context = canvas.getContext("2d");
-    var image = new Image();
-    image.src = "download.jpg";
-    image.onload = function (t) {
-        context.drawImage(image, 10, 10);
-        var idata = context.getImageData(0, 0, canvas.width, canvas.height);
-        var data = idata.data;
-        for (var i = 0; i <= data.length - 4; i += 4) {
-            data[i] = solarizeVal(data[i]);
-            data[i + 1] = solarizeVal(data[i + 1]);
-            data[i + 2] = solarizeVal(data[i + 2]);
-        }
-        context.putImageData(idata, 0, 0);
-    };
-    var SOLARIZE_LEVEL = 100;
-    function solarizeVal(v) {
-        if (v + SOLARIZE_LEVEL <= 255)
-            v = v + SOLARIZE_LEVEL;
-        else
-            v = 255;
-        return v;
-    }
-}, false);
+var imageData;
+var solarityLevelRangeCtrl = document.querySelector("#solarityLevel");
+var worker = new Worker("worker.js");
+solarityLevelRangeCtrl.onchange = function (event) {
+    worker.postMessage({
+        level: parseInt(solarityLevelRangeCtrl.value)
+    });
+};
+worker.onmessage = function (e) {
+    context.putImageData(e.data, 0, 0);
+};
+var IMAGE_Y_OFFSET = -600;
+canvas = document.getElementById("canvas");
+context = canvas.getContext("2d");
+var image = new Image();
+image.src = "download.jpg";
+image.onload = function () {
+    canvas.width = image.width;
+    canvas.height = image.height + IMAGE_Y_OFFSET;
+    context.drawImage(image, 0, IMAGE_Y_OFFSET, image.width, image.height);
+    imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+    worker.postMessage({ imageData: imageData });
+};
--- 2811/script.ts
+++ 2811/script.ts
@@ -1,39 +1,29 @@
 var canvas: HTMLCanvasElement;
-var context;
+var context: CanvasRenderingContext2D;
 
-window.addEventListener("load", () => {
-  var worker = new Worker("worker.js");
-  worker.onmessage = (e) => {
-    console.log("a");
-  };
-  worker.postMessage("abc");
-  
+var imageData;
+var solarityLevelRangeCtrl = <HTMLInputElement>document.querySelector("#solarityLevel");
+var worker = new Worker("worker.js");
+solarityLevelRangeCtrl.onchange = (event) => {
+  worker.postMessage({ 
+    level: parseInt(solarityLevelRangeCtrl.value)
+  });
+};
 
-  canvas = <HTMLCanvasElement>document.getElementById("canvas");
-  var context =canvas.getContext("2d");
-  var image = new Image();
-  image.src = "download.jpg";
-  image.onload = (t) => {
-    context.drawImage(image, 10, 10);
+worker.onmessage = (e) => {
+  context.putImageData(e.data, 0, 0);
+};
 
+var IMAGE_Y_OFFSET = -600;
+canvas = <HTMLCanvasElement>document.getElementById("canvas");
+context =canvas.getContext("2d");
+var image = new Image();
+image.src = "download.jpg";
+image.onload = () => {
+  canvas.width = image.width;
+  canvas.height = image.height + IMAGE_Y_OFFSET;
+  context.drawImage(image, 0, IMAGE_Y_OFFSET, image.width, image.height);
 
-    var idata = context.getImageData(0, 0, canvas.width, canvas.height);
-    var data = idata.data;
-    for (var i = 0; i <= data.length - 4; i += 4) {
-          data[i] = solarizeVal(data[i])
-          data[i + 1] = solarizeVal(data[i + 1])
-          data[i + 2] = solarizeVal(data[i + 2]);
-    }
-    context.putImageData(idata, 0, 0);
-  };
-
-var SOLARIZE_LEVEL = 100;
-  function solarizeVal(v) {
-    if(v + SOLARIZE_LEVEL <= 255)
-      v = v + SOLARIZE_LEVEL;
-    else
-      v = 255;
-    return v;
-  }
-
-}, false);
+  imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+  worker.postMessage({imageData: imageData});
+};
